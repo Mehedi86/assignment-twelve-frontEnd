619faa57868d62444f03db05758960ac7d4c5b20
Mehedi86 <abdullahalmehedi@gmail.com>
2025-05-09 20:41:38 +0600

    added: banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
     return (
-        <div className='h-[calc(100vh-64px)]'>
-            <img className='w-full object-center' src="/banner.jpg" alt="" />
+        <div className="h-[600px] bg-[url(/banner.jpg)] bg-center bg-cover flex items-center justify-center">
+            <div>
+                <div>
+                    <p className="text-black text-6xl font-bold">
+                        Welcome to Matrimony Service
+                    </p>
+                </div>
+                <h1 className="text-2xl font-bold text-black px-6 py-4 rounded-xl text-center">
+                    <TypeAnimation
+                        sequence={[
+                            'Welcome to Matrimony Service',
+                            2000,
+                            'Your journey to a lifelong bond begins here',
+                            2000,
+                            'Find the one who completes your story.',
+                            2000
+                        ]}
+                        speed={153}
+                        repeat={Infinity}
+                        wrapper="span"
+                    />
+                </h1>
+            </div>
         </div>
     );
 };
